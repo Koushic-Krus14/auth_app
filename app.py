fd16877ee8f6a5ec080ac56f1bb620f65f0238af
Koushic-Krus14 <117438000+Koushic-Krus14@users.noreply.github.com>
2025-07-24 02:39:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,5 @@ def image(image_id):
                      download_name=image_doc["filename"])
                      
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
